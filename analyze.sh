f2e0f78647a5b0106e7a5b5f2b38776165be674f
toioo <982903879@qq.com>
2024-03-13 09:57:37 +0800

    test fork puhsh

--- analyze.sh
+++ analyze.sh
@@ -1,14 +1,16 @@
 #/bin/bash
 
-if [ $# -eq 1 ];
+if [ $# -eq 1 ]
 then
     # Your code here. (1/4)
-	grep -E "WARN|ERROR" $1 >bug.txt
+	grep -E "WARN|ERROR|INFO" $1 > my_bug.txt
+	grep -E "WARN|ERROR" $1 > bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-	tail $1 -n 5
+	tail $1 -n 2 > 2.txt
+	tail $1 -n 5 > 5.txt
     ;;
     "--find")
         # Your code here. (3/4)
@@ -27,6 +29,7 @@ else
     "--diff")
         # Your code here. (4/4)
 	result=`diff $1 $3 -q`
+	#这里必须用 $? 因为 $? 获取的是命令的返回状态，我们这里比较的是diff命令的返回状态而不是 result的值
 	if [ $? -eq 0 ]
 	then
 		echo same
